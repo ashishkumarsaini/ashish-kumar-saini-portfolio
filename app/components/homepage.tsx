5a3e087c3a1e5ab0f7d522cfad961f178e612aa6
ashishkumarsaini <develop.ashishsaini@gmail.com>
2026-04-02 02:52:39 +0530

    added v2 flag

---
+++ app/components/homepage.tsx
@@ -0,0 +1,15 @@
+import { AboutSection } from "./about-section/about-section";
+import { HeroSection } from "./hero-section/hero-section";
+import { TestimonialSection } from "./testimonial-section/testimonial-section";
+
+const HomepageV2 = () => {
+  return (
+    <div>
+      <HeroSection />
+      <AboutSection />
+      <TestimonialSection />
+    </div>
+  )
+};
+
+export default HomepageV2;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+import { useSearchParams } from "next/navigation";
 import { AboutSection, ContactSection, ExperienceSection, HeroSection, SkillsSection } from "../components/sections";
 import "./globals.css";
-
+import HomepageV2 from "./components/homepage";
 
 export default function Home() {
+  const searchParams = useSearchParams();
+
+  if (searchParams.get('v2') === 'true') {
+    return <HomepageV2 />
+  }
+
   return (
     <div>
       <HeroSection />
