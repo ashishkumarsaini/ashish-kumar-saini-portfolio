f297178b9c422593f5b56d57c5771453ffd57c01
ashishkumarsaini <develop.ashishsaini@gmail.com>
2026-04-23 01:49:57 +0530

    implement new layout structure and add global styles; update tsconfig paths and add favicon

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter_Tight } from "next/font/google";
+import "./globals.css";
+// import { Header } from "@/components/header";
+// import { Footer } from "@/components/footer";
+
+const roboto = Inter_Tight({
+  subsets: ["latin"],
+  weight: ['400', '700']
+});
+
+export const metadata: Metadata = {
+  title: "Ashish Kumar Saini",
+  description: "Full Stack Developer (Next.js | React | Node.js | TypeScript) | 5+ Yrs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.className} antialiased`}
+      >
+        {/* <Header /> */}
+        <main className="max-w-[1500px] mx-auto px-4 sm:px-8">
+          {children}
+        </main>
+        {/* <Footer /> */}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import "./globals.css";
+
+export default function Home() {
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p>Comming Soon</p>
+    </div>
+  );
+}
