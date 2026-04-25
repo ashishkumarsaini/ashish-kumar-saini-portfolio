b2c2a5707e6e6c7e80600a36ca3bf62b5862b92c
ashishkumarsaini <develop.ashishsaini@gmail.com>
2026-04-26 02:58:32 +0530

    enhance BlogsPage layout and styling; add last updated timestamp and improve link presentation with border and padding

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,10 +1,11 @@
-import { Heading, Text } from "@/components/typography";
+import { Heading, HeadingSize, Text } from "@/components/typography";
 import Link from "next/link";
 
 const BlogsPage = async () => {
   const response = await fetch('https://gql.hashnode.com/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    next: { revalidate: 60 * 60 * 24 },
     body: JSON.stringify({
       query: `
         query Publication {
@@ -30,18 +31,22 @@ const BlogsPage = async () => {
 
   const result = await response.json();
   const posts = result.data?.publication?.posts.edges || [];
-
+  const now = new Date().toLocaleTimeString();
 
   return (
-    <div className="flex flex-col gap-4">
-      {posts.map(({ node }: { node: { id: string, url: string, title: string, brief: string } }) => (
-        <Link target="_blank" href={node.url} key={node.id} className="flex flex-col gap-2">
-          <Heading>
-            {node.title}
-          </Heading>
-          <Text>{node.brief}...</Text>
-        </Link>
-      ))}
+    <div className="max-w-5xl m-auto">
+      <Heading>Blogs</Heading>
+      <small>Last updated at {now}</small>
+      <div className="flex flex-col gap-4 mt-4">
+        {posts.map(({ node }: { node: { id: string, url: string, title: string, brief: string } }) => (
+          <Link target="_blank" href={node.url} key={node.id} className="flex flex-col gap-2 border rounded-md p-4">
+            <Heading size={HeadingSize.sm}>
+              {node.title}
+            </Heading>
+            <Text>{node.brief}...</Text>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 };
